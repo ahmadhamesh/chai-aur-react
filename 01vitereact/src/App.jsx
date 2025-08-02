f654f157e3991329d0f9d24191d6b06b3134641d
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2025-08-02 14:05:37 +0530

    custom react and use state

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,13 @@
 import Chai from "./chai"
 
 function App() {
-  
+  const username = "chai aur code"
 
   return (
     <>
       <h1>Chai aur React with Vite | Ahmad Hamesh</h1>
       <Chai />
+      <p>{username}</p>
     </>
     
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,54 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App !!</h1>
+    </div>
+  )
+}
+
+
+
+// const reactElement = {
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target: 'blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+//this can use as correct object that can call directly
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser ="chai aur reactjs"
+
+//this syntax is used by react that is created by react
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},
+  'click me to visit google',
+  anotherElement
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <MyApp />
+  // <App />
+  // reactElement
+
+  // anotherElement
+
+  // reactElement
+
+  // <App />
+
+  reactElement
+   
+  
 )
